cd2e6c5b02084442195e4d253780604efed9d523
phuongnga205 <mynga76262@gmail.com>
2026-07-30 14:44:06 +0700

    feat: init project NestjS + 1 api hello world + setup i18n + setup swagger

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Medium Clone API')
+    .setDescription('The Medium Clone API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Dùng cho chức năng đăng nhập sau này
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = 3000;
+  await app.listen(PORT);
+}
+bootstrap();
