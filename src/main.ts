ce92240b6a99c979d815d7d5259a808422d7432f
phuongnga205 <mynga76262@gmail.com>
2026-07-31 13:06:26 +0700

    fix: update .env and apply i18n for service

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
 }
 bootstrap();
